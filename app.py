0c6af037832412ed6ae5deecae2f819fbf6b04c9
beffaCo <cobe.91@hotmail.com>
2024-10-30 14:15:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ server = app.server
 
 app.layout = html.Div(
     [
-        html.H1("Welcome to the example dashboard: New deployment."),
+        html.H1("Welcome to the example dashboard: New deployment. Again"),
         html.Div("This dashboard will be deployed on kubernetes")
     ]
 )
